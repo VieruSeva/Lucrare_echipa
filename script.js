d1872e1ca43ee605bef37709123290d2c98bb832
VieruSeva <doflamingo2281@gmail.com>
2025-04-01 22:52:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const weatherData = {
   chisinau: { lat: 47.01, lon: 28.86 },
   balti: { lat: 47.76, lon: 27.93 },
@@ -7,6 +8,7 @@ const weatherData = {
 
 const API_KEY = '63a9e8c5334d9c0a0644cc370abc6d18'; 
 
+
 function getCurrentSeason() {
   const month = new Date().getMonth();
   if (month >= 2 && month <= 4) return 'spring';
@@ -15,10 +17,12 @@ function getCurrentSeason() {
   return 'winter';
 }
 
+
 document.addEventListener('DOMContentLoaded', function () {
   const citySelect = document.getElementById('city-select');
   const weatherResult = document.getElementById('weather-result');
   const currentSeason = getCurrentSeason();
+
 
   const currentSeasonCard = document.getElementById(currentSeason);
   if (currentSeasonCard) {
@@ -26,6 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
     currentSeasonCard.style.boxShadow = '0 10px 20px rgba(0, 0, 0, 0.15)';
   }
 
+ 
   citySelect.addEventListener('change', async function () {
     const selectedCity = this.value;
     const { lat, lon } = weatherData[selectedCity];
@@ -39,10 +44,12 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
+  
   citySelect.value = 'chisinau';
   const event = new Event('change');
   citySelect.dispatchEvent(event);
 
+  
   const seasonCards = document.querySelectorAll('.season-card');
   seasonCards.forEach(card => {
     card.addEventListener('mouseover', function () {
@@ -50,6 +57,7 @@ document.addEventListener('DOMContentLoaded', function () {
         this.style.transform = 'translateY(-10px)';
         this.style.boxShadow = '0 10px 20px rgba(0, 0, 0, 0.15)';
       } else {
+       
         this.style.transform = 'scale(1.1)';
         this.style.boxShadow = '0 12px 24px rgba(0, 0, 0, 0.2)';
       }
@@ -60,7 +68,7 @@ document.addEventListener('DOMContentLoaded', function () {
         this.style.transform = 'translateY(0)';
         this.style.boxShadow = '0 6px 12px rgba(0, 0, 0, 0.1)';
       } else {
- 
+       
         this.style.transform = 'scale(1.05)';
         this.style.boxShadow = '0 10px 20px rgba(0, 0, 0, 0.15)';
       }
@@ -68,17 +76,19 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+
 async function fetchRealTimeWeather(lat, lon) {
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
   const response = await fetch(url);
 
   if (!response.ok) {
-    throw new Error(HTTP error! Status: ${response.status});
+    throw new Error(`HTTP error! Status: ${response.status}`);
   }
 
   const data = await response.json();
   return data;
 }
+
 
 function displayWeather(weather, city) {
   const { temp, humidity } = weather.main;
